848a6b77c010af0fe9c380c80041a4d383033903
Jason Ekstrand <jason.ekstrand@intel.com>
2020-12-12 13:48:11 -0600

    Convert to using python-poppler

--- src/main/python/inputPDF.py
+++ src/main/python/inputPDF.py
@@ -14,18 +14,28 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
-from popplerqt5 import Poppler
+import poppler
 import PyPDF2
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, QSizeF
+from PyQt5.QtGui import QImage
 import shutil
 import tempfile
 import units
+
+POPPLER_TO_QT_FORMAT = {
+    poppler.ImageFormat.invalid: QImage.Format_Invalid,
+    poppler.ImageFormat.argb32: QImage.Format_ARGB32,
+    poppler.ImageFormat.bgr24: QImage.Format_BGR888,
+    poppler.ImageFormat.gray8: QImage.Format_Grayscale8,
+    poppler.ImageFormat.mono: QImage.Format_Mono,
+    poppler.ImageFormat.rgb24: QImage.Format_RGB888,
+}
 
 class InputPDFPage(object):
     def __init__(self, pdfFile, pageNumber):
         self.pdfFile = pdfFile
         self.pageNumber = pageNumber
-        self.page = pdfFile.doc.page(pageNumber - 1)
+        self.page = pdfFile.doc.create_page(pageNumber - 1)
         self._qImageSize = QSize(0, 0)
         self._qImage = None
 
@@ -42,8 +52,13 @@ class InputPDFPage(object):
     def getPyPDF2PageObject(self):
         return self.pdfFile.getPyPDF2Reader().getPage(self.pageNumber - 1)
 
+    def getSizeF(self):
+        rect = self.page.page_rect(poppler.PageBox.media_box)
+        assert rect.x == 0 and rect.y == 0
+        return QSizeF(rect.width, rect.height)
+
     def getSize(self):
-        return self.page.pageSize()
+        return self.getSizeF().toSize()
 
     def getQImage(self, sizeHint=None):
         if sizeHint == None:
@@ -54,19 +69,30 @@ class InputPDFPage(object):
         if self._qImageSize != sizeHint:
             self._qImage = None
 
-        while self._qImage is None:
-            xDpi = (sizeHint.width() * 72) / self.page.pageSizeF().width()
-            yDpi = (sizeHint.height() * 72) / self.page.pageSizeF().height()
-            image = self.page.renderToImage(xDpi, yDpi)
+        renderer = poppler.PageRenderer()
+        renderer.set_render_hint(poppler.RenderHint.antialiasing, True)
+        renderer.set_render_hint(poppler.RenderHint.text_antialiasing, True)
+        renderer.set_render_hint(poppler.RenderHint.text_hinting, True)
 
-            # If we ask to render an image that's too large, it will return
-            # an empty 1x1 image.  Contrary to what the Poppler docs say,
-            # it does not return a null image
-            if image.isNull() or image.size() == QSize(1, 1):
+        while self._qImage is None:
+            xDpi = (sizeHint.width() * 72) / self.getSizeF().width()
+            yDpi = (sizeHint.height() * 72) / self.getSizeF().height()
+            image = renderer.render_page(self.page, xDpi, yDpi)
+
+            qImage = QImage(image.data, image.width, image.height,
+                            image.bytes_per_row,
+                            POPPLER_TO_QT_FORMAT[image.format])
+
+            # If we ask Qt to import an image that's too large for it to
+            # handle, it will return an empty 1x1 image rather than a null
+            # image.
+            if qImage.isNull() or qImage.size() == QSize(1, 1):
                 sizeHint /= 2
                 continue
 
-            self._qImage = image
+            self._qImage = QImage(image.data, image.width, image.height,
+                                  image.bytes_per_row,
+                                  POPPLER_TO_QT_FORMAT[image.format])
             self._qImageSize = sizeHint
 
         return self._qImage
@@ -81,10 +107,7 @@ class InputPDFFile(object):
                                         os.path.basename(fileName))
         shutil.copyfile(fileName, self.tmpFileName)
 
-        self.doc = Poppler.Document.load(self.tmpFileName)
-        self.doc.setRenderHint(Poppler.Document.Antialiasing, True)
-        self.doc.setRenderHint(Poppler.Document.TextAntialiasing, True)
-        self.doc.setRenderHint(Poppler.Document.TextHinting, True)
+        self.doc = poppler.document.load_from_file(self.tmpFileName)
 
     def cleanup(self):
         self.tmpDir.cleanup()
@@ -93,7 +116,7 @@ class InputPDFFile(object):
         self.pdfReader = None
 
     def getNumPages(self):
-        return self.doc.numPages()
+        return self.doc.pages
 
     def getPage(self, pageNumber):
         return InputPDFPage(self, pageNumber)
